d62759f4c354d03a16de33d5d91850a792846a8c
Gia Bao <nguyengiabao16042005@gmail.com>
2025-12-17 17:38:07 +0700

    Initial commit for projects-milestones

---
+++ Models/Project.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectService.Models
+{
+    public class Project
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [StringLength(500)]
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public bool IsApproved { get; set; } = false;
+        public int CreatorId { get; set; }
+        public ICollection<Milestone> Milestones { get; set; }
+    }
+}
