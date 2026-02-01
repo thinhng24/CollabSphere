ad844da9d2a2bde18e4a45d5fa79fa80629d6431
hoangductrungg <152081877+hoangductrungg@users.noreply.github.com>
2026-02-01 21:45:54 +0700

    Delete backend/CollabSphere directory

--- backend/CollabSphere/CollabSphere.API/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CollabSphere.API.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string FullName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-    }
-}
