6c504464ab1e4d58191eebbca0129e63a10f7c0d
TrinhMinhVu <trinhminhvu.com.vn@gmail.com>
2026-02-01 21:42:18 +0700

    Update

---
+++ Backend/Services/CollabSphere.API/Controllers/ClassController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CollabSphere.API.Data;
+using CollabSphere.API.Models;
+
+namespace CollabSphere.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClassController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ClassController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var classes = await _context.Classes
+                .Include(c => c.Subject)
+                .ToListAsync();
+
+            return Ok(classes);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Class model)
+        {
+            _context.Classes.Add(model);
+            await _context.SaveChangesAsync();
+            return Ok(model);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cls = await _context.Classes.FindAsync(id);
+            if (cls == null) return NotFound();
+
+            _context.Classes.Remove(cls);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ Backend/Services/CollabSphere.API/Models/Class.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollabSphere.API.Models
+{
+    public class Class
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? ClassCode { get; set; }
+
+        [Required]
+        public string? ClassName { get; set; }
+
+        public string? LecturerName { get; set; }
+
+        public int StudentCount { get; set; }
+
+        public int SubjectId { get; set; }
+
+        public Subject? Subject { get; set; }   // 👈 BẮT BUỘC PHẢI CÓ
+    }
+}
