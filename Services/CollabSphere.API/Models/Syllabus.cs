f0fb2992af4abba292a211e3be6429208e390de5
TrinhMinhVu <trinhminhvu.com.vn@gmail.com>
2026-02-01 20:40:52 +0700

    Update

---
+++ Services/CollabSphere.API/Models/Syllabus.cs
@@ -0,0 +1,11 @@
+﻿namespace CollabSphere.API.Models
+{
+    public class Syllabus
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+        public required string Code { get; set; }
+        public string? Description { get; set; }
+    }
+}
